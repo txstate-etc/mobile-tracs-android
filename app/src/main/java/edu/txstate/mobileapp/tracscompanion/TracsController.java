33755f0271b3e7f8c8b063f7f347d489f4f54868
ajthyng <andrew.thyng@gmail.com>
2017-04-21 15:30:14 -0500

    User password is securely harvested
    I had to add a dependency on a third party library.
    I reviewed the source and tested the implementation
    and don't see any security concerns. Usernames and
    passwords are stored in an encrypted shared preferences
    file. The encryption secret is the users password, so
    the information is reset automatically every time it can't
    be read (i.e. the password changes).

--- app/src/main/java/edu/txstate/mobileapp/tracscompanion/TracsController.java
+++ app/src/main/java/edu/txstate/mobileapp/tracscompanion/TracsController.java
@@ -1,19 +1,21 @@
 package edu.txstate.mobileapp.tracscompanion;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.view.ViewTreeObserver;
+import android.content.SharedPreferences;
 import android.webkit.DownloadListener;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+
+import com.securepreferences.SecurePreferences;
 
 import edu.txstate.mobileapp.tracscompanion.util.FileDownloader;
 
 
-@SuppressLint("ViewConstructor")
-public class TracsController {
+class TracsController {
+    private static final String TAG = "TracsController";
+
     private String url;
-    private ViewTreeObserver.OnScrollChangedListener scrollChangedListener;
     private FileDownloader fileDownloader;
     private Context context;
     private WebView tracsView;
@@ -30,25 +32,40 @@ public class TracsController {
         this.tracsView.setWebViewClient(new TracsWebViewClient());
     }
 
-    public void downloadFile(String url, String mimetype) {
+    void downloadFile(String url, String mimetype) {
         this.fileDownloader.downloadFile(url, mimetype);
     }
 
-    public void javaScriptEnabled(boolean isEnabled) {
+    void javaScriptEnabled(boolean isEnabled) {
         this.tracsView.getSettings().setJavaScriptEnabled(isEnabled);
+        this.tracsView.addJavascriptInterface(this, "TracsController");
     }
 
-    public void zoomEnabled(boolean isEnabled) {
+    void zoomEnabled(boolean isEnabled) {
         this.tracsView.getSettings().setBuiltInZoomControls(isEnabled);
         this.tracsView.getSettings().setDisplayZoomControls(false);
     }
 
-    public void loadUrl() {
+    void loadUrl() {
         tracsView.loadUrl(this.url);
     }
 
-    public void setDownloadListener(DownloadListener downloadListener) {
+    void setDownloadListener(DownloadListener downloadListener) {
         this.tracsView.setDownloadListener(downloadListener);
+    }
+
+    @JavascriptInterface
+    public void deliver(String username, String password) {
+        SharedPreferences prefs = new SecurePreferences(this.context, password, "user_based_prefs.xml");
+        String currentPass = prefs.getString("password", "");
+
+        //A null object is returned if the stored info can't be decrypted.
+        if (currentPass == null) { currentPass = ""; }
+
+        if (!currentPass.equals(password)) {
+            prefs.edit().putString("username", username).apply();
+            prefs.edit().putString("password", password).apply();
+        }
     }
 
     private class TracsWebViewClient extends WebViewClient {
@@ -56,6 +73,27 @@ public class TracsController {
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             view.loadUrl(url);
             return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            if (url.contains("login.its.txstate.edu")) {
+                String javascript = "document.getElementsByTagName('form')[0].onsubmit = function() {\n" +
+                        "\tvar username, password;\n" +
+                        "\tvar inputs = document.getElementsByTagName('input');\n" +
+                        "\tfor (var i = 0; i < inputs.length; i++) {\n" +
+                        "\t\tif (inputs[i].name.toLowerCase() === 'password') {\n" +
+                        "\t\t\tpassword = inputs[i] == null ? \"\" : inputs[i].value;\n" +
+                        "\t\t} else if (inputs[i].name.toLowerCase() === 'username') {\n" +
+                        "\t\t\tusername = inputs[i] === null ? \"\" : inputs[i].value;\n" +
+                        "\t\t}\n" +
+                        "\t}\n" +
+                        "\twindow.TracsController.deliver(username, password);\n" +
+                        "\treturn true;\n" +
+                        "}";
+                view.loadUrl("javascript:" + javascript);
+            }
         }
     }
 }
