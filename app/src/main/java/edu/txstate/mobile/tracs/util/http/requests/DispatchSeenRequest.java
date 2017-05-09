68296912f8821dcb44543a2327151e1f01813f6a
ajthyng <andrew.thyng@gmail.com>
2017-05-09 08:39:33 -0500

    Created request to mark object as seen

--- app/src/main/java/edu/txstate/mobile/tracs/util/http/requests/DispatchSeenRequest.java
+++ app/src/main/java/edu/txstate/mobile/tracs/util/http/requests/DispatchSeenRequest.java
@@ -6,22 +6,44 @@ import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import edu.txstate.mobile.tracs.notifications.DispatchNotification;
 
 public class DispatchSeenRequest extends Request<Void> {
 
+    private static final String responseError = "Error communicating with dispatch";
     private static final String TAG = "DispatchSeenRequest";
+    private String notificationId;
 
-    public DispatchSeenRequest(int method, String url) {
-        super(method, url, error -> Log.wtf(TAG, error.getMessage()));
+    public DispatchSeenRequest(String url, DispatchNotification dispatchNotification) {
+        super(Method.PATCH, url + "/" + dispatchNotification.getId(), error -> Log.wtf(TAG, error.getMessage()));
+    }
+
+    @Override
+    public byte[] getBody() {
+        JSONObject body = new JSONObject();
+        try {
+            body.put("seen", true);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return body.toString().getBytes();
     }
 
     @Override
     protected Response<Void> parseNetworkResponse(NetworkResponse response) {
-        return null;
+        if (response.statusCode == 200) {
+            return Response.success(null, HttpHeaderParser.parseCacheHeaders(response));
+        }
+        return Response.error(new VolleyError(responseError));
     }
 
     @Override
     protected void deliverResponse(Void response) {
-
+        //TODO: Log firebase event for notification marked as seen
     }
 }
