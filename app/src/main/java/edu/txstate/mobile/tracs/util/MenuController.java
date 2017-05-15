b9ccbe97b5a122b13d7d34383f94b1f80eb21587
ajthyng <andrew.thyng@gmail.com>
2017-05-15 10:04:40 -0500

    It felt awkward having a back stack with duplicates
    When a menu item is pressed, if you are currently on the same
    activity then a new instance of that activity is launched, but
    the back stack is set to the manifest defined parent.

--- app/src/main/java/edu/txstate/mobile/tracs/util/MenuController.java
+++ app/src/main/java/edu/txstate/mobile/tracs/util/MenuController.java
@@ -1,5 +1,6 @@
 package edu.txstate.mobile.tracs.util;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
@@ -9,23 +10,37 @@ import edu.txstate.mobile.tracs.R;
 import edu.txstate.mobile.tracs.SettingsActivity;
 
 public class MenuController {
+    private static final String MAIN_ACTIVITY = "MainActivity";
+    private static final String NOTIFICATIONS_ACTIVITY = "NotificationActivity";
+    private static final String SETTINGS_ACTIVITY = "SettingsActivity";
+
     public static boolean handleMenuClick(int menuId, Context context) {
         switch (menuId) {
             case R.id.menu_notifications:
                 Intent notificationsIntent = new Intent(context, NotificationsActivity.class);
+                checkBackStack(notificationsIntent, context, NOTIFICATIONS_ACTIVITY);
                 context.startActivity(notificationsIntent);
                 break;
             case R.id.menu_settings:
                 Intent settingsIntent = new Intent(context, SettingsActivity.class);
+                checkBackStack(settingsIntent, context, SETTINGS_ACTIVITY);
                 context.startActivity(settingsIntent);
                 break;
             case R.id.menu_home:
                 Intent homeIntent = new Intent(context, MainActivity.class);
+                checkBackStack(homeIntent, context, MAIN_ACTIVITY);
                 context.startActivity(homeIntent);
                 break;
             default:
                 return false;
         }
         return true;
+    }
+
+    private static void checkBackStack(Intent intent, Context context, String menuName) {
+        String callingActivity = Activity.class.cast(context).getLocalClassName();
+        if (callingActivity.equals(menuName)) {
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
     }
 }
