8efa08e8ce815855215dd12638bd8c8cf4efca7a
ajthyng <andrew.thyng@gmail.com>
2017-05-17 08:49:45 -0500

    TXST menu option opens dublabs app or playstore

--- app/src/main/java/edu/txstate/mobile/tracs/util/MenuController.java
+++ app/src/main/java/edu/txstate/mobile/tracs/util/MenuController.java
@@ -1,9 +1,12 @@
 package edu.txstate.mobile.tracs.util;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
+import edu.txstate.mobile.tracs.AnalyticsApplication;
 import edu.txstate.mobile.tracs.MainActivity;
 import edu.txstate.mobile.tracs.NotificationsActivity;
 import edu.txstate.mobile.tracs.R;
@@ -13,6 +16,8 @@ public class MenuController {
     private static final String MAIN_ACTIVITY = "MainActivity";
     private static final String NOTIFICATIONS_ACTIVITY = "NotificationActivity";
     private static final String SETTINGS_ACTIVITY = "SettingsActivity";
+
+    private static final String DUBLABS_APP = "edu.txstate.mobileapp";
 
     public static boolean handleMenuClick(int menuId, Context context) {
         switch (menuId) {
@@ -30,6 +35,18 @@ public class MenuController {
                 Intent homeIntent = new Intent(context, MainActivity.class);
                 checkBackStack(homeIntent, context, MAIN_ACTIVITY);
                 context.startActivity(homeIntent);
+                break;
+            case R.id.menu_dublabs:
+                Intent dubLabsIntent = context.getPackageManager().getLaunchIntentForPackage(DUBLABS_APP);
+                if (dubLabsIntent != null) { //Dublabs app is installed
+                    context.startActivity(dubLabsIntent);
+                } else { //Dublabs app was not installed
+                    try { //to go to the play store and get it
+                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + DUBLABS_APP)));
+                    } catch (ActivityNotFoundException e) { //the play store isn't installed
+                        context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + DUBLABS_APP)));
+                    }
+                }
                 break;
             default:
                 return false;
