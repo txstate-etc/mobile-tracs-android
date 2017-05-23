0d3b2161095770606ed7b9d3253bfedfbfe97d00
ajthyng <andrew.thyng@gmail.com>
2017-05-23 10:43:04 -0500

    Added no internet page in case user has no internet

---
+++ app/src/main/java/edu/txstate/mobile/tracs/util/PageLoader.java
@@ -0,0 +1,57 @@
+package edu.txstate.mobile.tracs.util;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import edu.txstate.mobile.tracs.AnalyticsApplication;
+
+@SuppressWarnings("ALL")
+public class PageLoader {
+    private static final PageLoader pageLoader = new PageLoader();
+    private static final String TAG = "PageLoader";
+    public static PageLoader getInstance() {
+        return pageLoader;
+    }
+
+    private PageLoader() {
+    }
+
+    public String loadHtml(String filename) {
+        InputStream input = null;
+        String returnString = null;
+        try {
+            input = AnalyticsApplication.getContext().getAssets().open(filename);
+        } catch (IOException e) {
+            Log.wtf(TAG, "Couldn't open html file");
+            returnString = "";
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        String line;
+        StringBuilder stringBuilder = new StringBuilder();
+        String ls = System.getProperty("line.separator");
+
+        try {
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(ls);
+            }
+            returnString = stringBuilder.toString();
+        } catch(IOException e) {
+            Log.wtf(TAG, "Couldn't read html file");
+            returnString = "";
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                Log.wtf(TAG, "File was not open");
+                returnString = "";
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
