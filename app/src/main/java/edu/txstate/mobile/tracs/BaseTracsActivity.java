81e7658bde088246d14b78d260d9263146faddac
ajthyng <andrew.thyng@gmail.com>
2017-06-01 17:06:55 -0500

    Added normal and large portrait layouts for about app

--- app/src/main/java/edu/txstate/mobile/tracs/BaseTracsActivity.java
+++ app/src/main/java/edu/txstate/mobile/tracs/BaseTracsActivity.java
@@ -1,6 +1,7 @@
 package edu.txstate.mobile.tracs;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
