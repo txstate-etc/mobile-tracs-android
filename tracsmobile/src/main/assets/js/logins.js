8122ec95b5a19ca997dc510cd2b718cfac6ef9f6
ajthyng <andrew.thyng@gmail.com>
2017-06-20 15:55:40 -0500

    Trim username before submission to CAS

--- tracsmobile/src/main/assets/js/logins.js
+++ tracsmobile/src/main/assets/js/logins.js
@@ -1,11 +1,12 @@
 document.getElementsByTagName('form')[0].onsubmit = function() {
     var username, password, publicStation;
     var inputs = document.getElementsByTagName('input');
-    var publicStation = document.getElementsByNam
+    var publicStation = document.getElementsByName('publicWorkstation')[0].checked
     for (var i = 0; i < inputs.length; i++) {
         if (inputs[i].name.toLowerCase() === 'password') {
             password = inputs[i] === null ? '' : inputs[i].value;
         } else if (inputs[i].name.toLowerCase() === 'username') {
+            inputs[i].value = inputs[i].value.trim();
             username = inputs[i] === null ? '' : inputs[i].value;
         } else if (inputs[i].name === 'publicWorkstation') {
             publicStation = inputs[i].checked;
