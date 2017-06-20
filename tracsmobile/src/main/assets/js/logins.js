878999c3be5ec26f2bcaf9e3af11083fdea516cd
ajthyng <andrew.thyng@gmail.com>
2017-06-20 16:13:19 -0500

    Tweaked javascript to remove for loop

--- tracsmobile/src/main/assets/js/logins.js
+++ tracsmobile/src/main/assets/js/logins.js
@@ -1,18 +1,7 @@
 document.getElementsByTagName('form')[0].onsubmit = function() {
-    var username, password, publicStation;
-    var inputs = document.getElementsByTagName('input');
+    var username = document.getElementsByName('username')[0].value.toLowerCase().trim();
+    var password = document.getElementsByName('password')[0].value;
     var publicStation = document.getElementsByName('publicWorkstation')[0].checked
-    for (var i = 0; i < inputs.length; i++) {
-        if (inputs[i].name.toLowerCase() === 'password') {
-            password = inputs[i] === null ? '' : inputs[i].value;
-        } else if (inputs[i].name.toLowerCase() === 'username') {
-            inputs[i].value = inputs[i].value.trim();
-            username = inputs[i] === null ? '' : inputs[i].value;
-        } else if (inputs[i].name === 'publicWorkstation') {
-            publicStation = inputs[i].checked;
-        }
-    }
     TracsWebView.deliver(username, password, !publicStation);
-
     return true;
 }
