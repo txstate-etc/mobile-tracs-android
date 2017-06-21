2a5be077d19788c7ca0bcdad4c553d0c4a355ab2
ajthyng <andrew.thyng@gmail.com>
2017-06-21 13:57:05 -0500

    Fixed click action on push notification not launching notifications screen

--- tracsmobile/src/main/java/edu/txstate/mobile/tracs/Splash.java
+++ tracsmobile/src/main/java/edu/txstate/mobile/tracs/Splash.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class Splash extends AppCompatActivity {
+    private final String extraTitle = "shouldLoadNotificationsView";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String shouldLoadNotifications = getIntent().getStringExtra(extraTitle);
         Intent mainActivity = new Intent(Splash.this, MainActivity.class);
+        mainActivity.putExtra(extraTitle, shouldLoadNotifications);
         startActivity(mainActivity);
         finish();
     }
