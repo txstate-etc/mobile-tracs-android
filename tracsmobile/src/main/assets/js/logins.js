1fa0892639376a5bb2cd5d4ddb11ec4351c405ab
ajthyng <andrew.thyng@gmail.com>
2017-06-26 13:14:36 -0500

    Fixed trim issue with login - Ticket 7845

--- tracsmobile/src/main/assets/js/logins.js
+++ tracsmobile/src/main/assets/js/logins.js
@@ -1,5 +1,5 @@
 document.querySelector('form').onsubmit = function() {
-    var username = document.querySelector('form input[name="username"]').value.toLowerCase().trim();
+    var username = document.querySelector('form input[name="username"]').value = document.querySelector('form input[name="username"]').value.toLowerCase().trim();
     var password = document.querySelector('form input[name="password"]').value;
     document.querySelector('form input[name="publicWorkstation"]').checked = false;
     TracsWebView.deliver(username, password);
